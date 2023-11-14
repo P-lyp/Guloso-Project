ef3e93db43f7d1b471f93834bd395f7a87bca993
Ruan Guedes de Souza <ruanguedes.info@gmail.com>
2023-11-14 10:53:13 -0400

    adaptação Session para consumir o backend

--- client/src/components/session/session.jsx
+++ client/src/components/session/session.jsx
@@ -1,16 +1,47 @@
-import React from "react";
-import dados from "../../assets/dados.json";
+import React, { useEffect, useState } from "react";
 import './session.css';
 import paidIcon from '../../assets/Done.svg'
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5000");
 
 function Session() {
-    console.table(dados)
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        socket.on('showData', (data) => {
+            setData(data);
+        });
+
+        return () => {
+            socket.off('dadosAtualizados');
+        };
+    },[]);
+
+    console.table(data)
+
+
     return(
+
+        // ANTIGA FORMA
+
+        // <div className="session">
+        //     <h1 className="title">Lista de mesas</h1>
+        //     <div className="session-list">
+        //     {dados.map(localSession => (
+        //         <SessionItem key={localSession.id} list={localSession.id} ocupada={localSession.ocupada} pago={localSession.pago}/>
+        //     ))}
+        //     </div>
+        // </div>
+
+        
+        // NOVA FORMA
+
         <div className="session">
             <h1 className="title">Lista de mesas</h1>
             <div className="session-list">
-            {dados.map(localSession => (
-                <SessionItem key={localSession.id} list={localSession.id} ocupada={localSession.ocupada} pago={localSession.pago}/>
+            {data && data.map(item => (
+                <SessionItem key={item.id} list={item.id} ocupada={item.taken} pago={item.paid}/>
             ))}
             </div>
         </div>
@@ -18,8 +49,8 @@ function Session() {
 }
 
 function SessionItem({list, ocupada, pago}) {
-    const badgeStyle = {width: '15px', height: '15px', position: 'absolute', top: 0, right: 0};
 
+    const badgeStyle = {width: '15px', height: '15px', position: 'absolute', top: 0, right: 0};
     const sessionItem = {backgroundColor: ocupada ? '#EBE3D5' : '#B0A695'}
 
     return (
