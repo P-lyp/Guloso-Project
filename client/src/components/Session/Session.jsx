95ffd4fad46a5977d884eab01179902cbe057260
p-lyp <felipealves13tga@hotmail.com>
2024-02-10 22:17:14 -0400

    pfv Deus

---
+++ client/src/components/Session/Session.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import "./Session.css";
+import paidIcon from "../../assets/Done.svg";
+
+import socketServer from "../../socket";
+
+function Session() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        socketServer.on("showData", (data) => {
+            setData(data);
+        });
+
+        return () => {
+            socketServer.off("dadosAtualizados");
+        };
+    }, []);
+
+    return (
+        // ANTIGA FORMA
+
+        // <div className="session">
+        //     <h1 className="title">Lista de mesas</h1>
+        //     <div className="session-list">
+        //     {dados.map(localSession => (
+        //         <SessionItem key={localSession.id} list={localSession.id} ocupada={localSession.ocupada} pago={localSession.pago}/>
+        //     ))}
+        //     </div>
+        // </div>
+
+        // NOVA FORMA
+
+        <div className="session">
+            <h1 className="title">Lista de mesas</h1>
+            <div className="session-list">
+                {data &&
+                    data.map((item) => (
+                        <SessionItem
+                            key={item.id}
+                            list={item.id}
+                            ocupada={item.taken}
+                            pago={item.paid}
+                        />
+                    ))}
+            </div>
+        </div>
+    );
+}
+
+function SessionItem({ list, ocupada, pago }) {
+    const badgeStyle = {
+        width: "15px",
+        height: "15px",
+        position: "absolute",
+        top: 0,
+        right: 0,
+    };
+    const sessionItem = { backgroundColor: ocupada ? "#EBE3D5" : "#B0A695" };
+
+    return (
+        <div className="session-item" style={sessionItem}>
+            <p>{list}</p>
+            {pago && <img src={paidIcon} alt="Pago" style={badgeStyle} />}
+        </div>
+    );
+}
+
+export default Session;
